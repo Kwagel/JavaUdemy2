ef2a520136a52a5c1a0e2841fae3340a93e7f3c7
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-05 15:20:23 +0000

    synchronisation

--- src/MultipleThreads/Main.java
+++ src/MultipleThreads/Main.java
@@ -3,6 +3,7 @@ package MultipleThreads;
  class Main {
 	public static void main(String[] args) {
 	Countdown countdown = new Countdown();
+
 	CountdownThread t1 = new CountdownThread(countdown);
 			t1.setName("Thread 1");
 	CountdownThread t2 = new CountdownThread(countdown);
@@ -15,8 +16,10 @@ package MultipleThreads;
 
 class Countdown {
 //	 in this instance, the thread share the variable on the heap, and since running is in a random order, the results cannot be consistent
+//	when threads fight for usage of the same variable, it causes race conditions
 		private int i;
-	public void doCountdown() {
+//		synchronized makes a whole thread run to completion before allowing other threads to access it
+	public  void doCountdown() {
 		String colour;
 		switch (Thread.currentThread().getName()) {
 			case "Thread 1":
@@ -28,8 +31,13 @@ class Countdown {
 			default:
 				colour = ThreadColour.ANSI_GREEN;
 		}
-		for ( i = 10; i > 0; i--) {
-			System.out.println(colour + Thread.currentThread().getName() + ": i = " + i);
+//		variables have a lock feature, which means that threads will have to compete for it, however for local variables, they aare created separately, so they won't have functionality if used unless its on a variable the threads  actually share
+//		Strings are unique and can be used to synchronise threads as JVM using string pools which are reused
+		synchronized (this) {
+//			you can synchronise to the method itself. which is referred to using this
+			for (i = 10; i > 0; i--) {
+				System.out.println(colour + Thread.currentThread().getName() + ": i = " + i);
+			}
 		}
 	}
 }
