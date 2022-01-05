2770a0e4b69fdb63e0350dcc48ee31645c279226
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-05 14:20:32 +0000

    Thread Join and Interrupt

--- src/Threads/AnotherThread.java
+++ src/Threads/AnotherThread.java
@@ -1,12 +1,20 @@
 package Threads;
 
-public class AnotherThread extends Thread{
+import static Threads.ThreadColour.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
 	
 	@Override
 	public void run() {
-		System.out.println("Hello from another thread");
-		System.out.println("Hello from another thread");
-		System.out.println("Hello from another thread");
-		System.out.println("Hello from another thread");
+		System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+		
+		try {
+			Thread.sleep(3000);
+			
+		} catch (InterruptedException e) {
+			System.out.println(ANSI_BLUE + " Another thread woke me up");
+			return;
+		}
+		System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
 	}
 }
--- src/Threads/Main.java
+++ src/Threads/Main.java
@@ -1,14 +1,43 @@
 package Threads;
 
+import static Threads.ThreadColour.*;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello from the main thread");
+		System.out.println(ANSI_PURPLE +"Hello from the main thread");
 		Thread anotherThread = new AnotherThread();
+		anotherThread.setName("== Another Thread ==");
+		anotherThread.start();
 //		invoke with start, runs the run method for a thread
 //		threads can only be started once
-		anotherThread.start();
-//		Threads run in a random order,
-		System.out.println("Hello Again from the main thread");
+//		you have to use start as if you use run(), you will actually run the code on the same thread
+//		anotherThread.run();
+		
+		new Thread(){
+			public void run(){
+				System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
+			}
+		}.start();
+//		Can create runnable classes my implementing runnable
+		Thread myRunnableThread = new Thread(new MyRunnable(){
+			@Override
+			public void run() {
+				System.out.println(ANSI_RED + "Hello from the anonymous clas's implementation of run()");
+				try{
+//					you can add a millisecond value to time out the join command after a certain amount of time has passed.
+					anotherThread.join();
+					System.out.println(ANSI_RED + "Another Thread terminated or timed out, so I'm running again");
+				}catch (InterruptedException e ){
+					System.out.println(ANSI_RED + " I couldn't wait after all. I was interrupted");
+				}
+			}
+		});
+		myRunnableThread.start();
+//		anotherThread.interrupt();
+		
+//		Threads run in a random order, working with threads is tricky
+		System.out.println(ANSI_PURPLE +"Hello again from the main thread");
+		
 		
 	}
 }
