3f8c46247c9dc3855e4b4c4f15b71965cb91f02f
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-05 16:04:37 +0000

    deadlocks,wait,notify and notifyall

--- src/Messages/Main.java
+++ src/Messages/Main.java
@@ -4,7 +4,13 @@ import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
-	
+//		when one methodd is waiting for access of another variable, this is known as a deadlock
+//		atomic operations that cannot be stopped by a thread
+//		reading or writing reference variables, reading or writing primitive variables except double or long(which can be split between two runs), and volatile variables
+		Message message = new Message();
+//		some collections aren't thread safe, not natively synchronised, so you must synchronise it when using it
+		new Thread(new Writer(message)).start();
+		new Thread(new Reader(message)).start();
 	}
 }
 
@@ -14,17 +20,30 @@ class Message {
 	
 	public synchronized String read() {
 		while (empty) {
-		
+			try {
+//				when not using the message variable, it waits and releases the lock to allow other threads to use it
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println();
+			}
 		}
 		empty = true;
+		notifyAll();
 		return message;
 	}
 	
 	public synchronized void write(String message) {
+//		the wait() is in a while loop testing for the condition that the method is waiting for, threads can be woken up without fulfilling conditions, so it must be constantly tested for while the thread is awake
 		while (!empty) {
-		
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println();
+			}
 		}
 		empty = false;
+//		you cannot notify a specific thread( notify doesn't accept any parameters
+		notifyAll();
 		this.message = message;
 	}
 }
@@ -48,12 +67,33 @@ class Writer implements Runnable {
 		
 		for (int i = 0; i < messages.length; i++) {
 			message.write(messages[i]);
-			try{
+			try {
 				Thread.sleep(random.nextInt(2000));
-			}catch (InterruptedException e){
+			} catch (InterruptedException e) {
 				System.out.println();
 			}
 		}
 		message.write("Finished");
+	}
+}
+
+class Reader implements Runnable {
+	private Message message;
+	
+	public Reader(Message message) {
+		this.message = message;
+	}
+	
+	public void run() {
+		Random random = new Random();
+		for (String latestMessage = message.read(); !latestMessage.equals("Finished"); latestMessage = message.read()) {
+			System.out.println(latestMessage);
+			try {
+				Thread.sleep(random.nextInt(2000));
+				
+			} catch (InterruptedException e) {
+				System.out.println();
+			}
+		}
 	}
 }
