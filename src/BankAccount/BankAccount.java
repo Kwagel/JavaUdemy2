23a9bdc71a8e136415778e76dd40749aad5a74a1
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-06 16:32:26 +0000

    Challenge 3,4 and 5 - Using trylock, synchronize, intelligent locking to not waste resources

--- src/BankAccount/BankAccount.java
+++ src/BankAccount/BankAccount.java
@@ -1,12 +1,18 @@
 package BankAccount;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 	private String accountNumber;
 	private double balance;
+	private Lock lock;
 	
 	public BankAccount(String accountNumber, double balance) {
 		this.accountNumber = accountNumber;
 		this.balance = balance;
+		lock = new ReentrantLock();
 	}
 	
 	//	public synchronized void deposit(double amount){
@@ -20,17 +26,47 @@ public class BankAccount {
 	public void deposit(double amount) {
 //		this method is better when the code is less simple and only critical code need to be synchronised instead of the entre method
 //		this is to ensure best performance
-		synchronized (this) {
-			
-			balance += amount;
+		try {
+			if (lock.tryLock(1000, TimeUnit.MILLISECONDS)){
+				try {
+					
+					balance += amount;
+				} finally {
+					
+					lock.unlock();
+				}
+			}else{
+				System.out.println("Could not get the lock");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
+		
 	}
 	
 	public void withdraw(double amount) {
-		synchronized (this) {
-			balance -= amount;
-			
+		try {
+			if (lock.tryLock(1000,TimeUnit.MILLISECONDS)){
+			try {
+				balance -= amount;
+			} finally {
+				lock.unlock();
+			}
+			}else{
+				System.out.println("Could not get the lock");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public void printAccountNumber() {
+		System.out.println("Account number = " + accountNumber);
+		
 	}
 	
 	public double getBalance() {
