5644691b3b1187b89ca7604b97a4607710d7c787
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-07 14:27:22 +0000

    Usage of consumer, Unary operators, functions and bifunctions and how to chain them together

--- src/Employees/Main.java
+++ src/Employees/Main.java
@@ -2,13 +2,13 @@ package Employees;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
+
 
 public class Main {
 	public static void main(String[] args) {
@@ -27,31 +27,38 @@ public class Main {
 		employees.add(snow);
 		employees.add(red);
 		employees.add(charming);
-		
+
 //		by writing functions, you can do similar calculations on code, without having to use interfaces to employ methods
 //		by passing functions with the same generics parameters, but different names, a method can accept
 		
-		Function<Employee, String> getLastName = (Employee employee) -> {
-			return employee.getName().substring(employee.getName().indexOf(" ") + 1);
-		};
+		Function<Employee, String> getLastName = (Employee employee) -> employee.getName().substring(employee.getName().indexOf(" ") + 1);
+		
 		
 		String lastname = getLastName.apply(employees.get(1));
 		System.out.println(lastname);
 		
-		Function<Employee, String> getFirstName = (Employee employee) -> {
-			return employee.getName().substring(0, employee.getName().indexOf(" "));
-		};
-		String firstname = getFirstName.apply(employees.get(1));
+		Function<Employee, String> getFirstName = (Employee employee) -> employee.getName().substring(0, employee.getName().indexOf(" "));
+		
 
 		
 		Random random = new Random();
 		for (Employee employee : employees) {
-			if(random.nextBoolean()){
-				System.out.println(getAName(getFirstName,employee));
-			}else {
-				System.out.println(getAName(getLastName,employee));
+			if (random.nextBoolean()) {
+				System.out.println(getAName(getFirstName, employee));
+			} else {
+				System.out.println(getAName(getLastName, employee));
 			}
 		}
+		
+//		UnaryOperators take an argument and return a value of the same type as the argument
+		
+		IntUnaryOperator intBy5 = i -> i +5;
+		System.out.println(intBy5.applyAsInt(10));
+		
+		Consumer<String> c1 = s -> s.toUpperCase();
+		Consumer<String> c2  = s -> System.out.println(s);
+//		accept runs on the method that called the andThen, not the argument inside the andThen
+		c1.andThen(c2).accept("Hello, World!");
 /*
 
 		How ForEach works
@@ -108,9 +115,11 @@ public class Main {
 //			System.out.println("Last name is " + lastName);
 //		});
 	}
+	
 	private static String getAName(Function<Employee, String> getName, Employee employee) {
 		return getName.apply(employee);
 	}
+	
 	public static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition) {
 		System.out.println(ageText);
 		System.out.println("=====================");
