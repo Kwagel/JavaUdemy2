74a5e5864e0406d0556d85424f002d01f254a591
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-12 10:51:25 +0000

    JUnit challenges continued, using argument Streams to populate Parameterized tests using a method static method source

--- src/JUnitChallenge/UtilitiesTest.java
+++ src/JUnitChallenge/UtilitiesTest.java
@@ -1,42 +1,75 @@
 package JUnitChallenge;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilitiesTest {
+	private Utilities util;
+	
+	@BeforeEach
+	public void setup() {
+		util = new Utilities();
+	}
 	
 	@Test
 	void everyNthChar() {
-		Utilities util = new Utilities();
-		assertArrayEquals(new char[]{'e', 'l'}, util.everyNthChar(new char[]{'h','e','l','l','o'}, 2) );
+		assertArrayEquals(new char[]{'e', 'l'}, util.everyNthChar(new char[]{'h', 'e', 'l', 'l', 'o'}, 2));
 	}
 	
 	@Test
 	void everyNthChar_NGreaterThanLength() {
-		Utilities util = new Utilities();
-		assertArrayEquals(new char[]{'h','e','l','l','o'}, util.everyNthChar(new char[]{'h','e','l','l','o'}, 6) );
+		assertArrayEquals(new char[]{'h', 'e', 'l', 'l', 'o'}, util.everyNthChar(new char[]{'h', 'e', 'l', 'l', 'o'}, 6));
 	}
+	
 	@Test
 	void removePairs() {
-		Utilities util = new Utilities();
 		assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
 		assertEquals("ABCABDEF", util.removePairs("ABCCABDEFF"));
-		assertNull( util.removePairs(null),"Did not return null when receiving null");
-		assertEquals("A",util.removePairs("A"));
-		assertEquals("",util.removePairs(""));
+		assertNull(util.removePairs(null), "Did not return null when receiving null");
+		assertEquals("A", util.removePairs("A"));
+		assertEquals("", util.removePairs(""));
 	}
 	
 	@Test
 	void converter() {
-		Utilities util = new Utilities();
-		assertEquals(300, util.converter(10,5) );
+		assertEquals(300, util.converter(10, 5));
+	}
+	
+	@Test
+	void converter_DivideBy0() {
+		assertThrows(ArithmeticException.class, () -> util.converter(10, 0)
+		);
+		
 	}
 	
 	@Test
 	void nullIfOddLength() {
-		Utilities util = new Utilities();
 		assertNull(util.nullIfOddLength("odd"));
-		assertEquals("even",util.nullIfOddLength("even"));
+		assertEquals("even", util.nullIfOddLength("even"));
+	}
+	
+	@ParameterizedTest
+	@MethodSource("testData")
+	void removePairs_Many(String Input, String output) {
+		assertEquals(output, util.removePairs(Input));
+	}
+	
+	
+	public static Stream<Arguments> testData() {
+		return Stream.of(
+				Arguments.of("ABCDEFF", "ABCDEF"),
+				Arguments.of("AB88EFFG", "AB8EFG"),
+				Arguments.of("112233445566", "123456"),
+				Arguments.of("ZYZQQB", "ZYZQB"),
+				Arguments.of("A", "A")
+		);
+		
 	}
 }
