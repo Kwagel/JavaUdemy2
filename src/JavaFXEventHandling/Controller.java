2c2fbceddbf144234e9746e50795daaa161316d4
Kenneth Lau <kennethlau0759@gmail.com>
2022-01-18 13:07:17 +0000

    Assigning fx:id to buttons, and using them so the method does different stuff. By giving the method ActionEvent e, you can check what button was pressed and do different things depending on that. As well as this, you can detect and run a method after key-press, to check if a text-field is empty or not, and prevent buttons from being pressed while it is empty or full of white pace. This is done using setDisable, and passing a boolean value that detects if the getText() is empty or empty when trimmed.

--- src/JavaFXEventHandling/Controller.java
+++ src/JavaFXEventHandling/Controller.java
@@ -1,17 +1,47 @@
 package JavaFXEventHandling;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 
 public class Controller {
 	
+	
+	@FXML
+	public Button byeButton;
+	@FXML
+	public Button helloButton;
 	@FXML
 	private TextField nameField;
 	
-//	not need but is good for debugging as it shows that a method is used on the FMXL fil
 	@FXML
-	public void onButtonClicked() {
-		System.out.println("Hello " + nameField.getText());
+	public void initialize(){
+		helloButton.setDisable(true);
+		byeButton.setDisable(true);
 	}
+	
+	@FXML
+	public void onButtonClicked(ActionEvent e) {
+		if (e.getSource().equals(helloButton)) {
+			
+			System.out.println("Hello " + nameField.getText());
+		} else if (e.getSource().equals(byeButton)) {
+			System.out.println("Bye " + nameField.getText());
+		}
+		System.out.println("The following button was pressed: " + e.getSource());
+		
+	}
+	
+	@FXML
+	public void handleKeyReleased(){
+		String text = nameField.getText();
+		boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+		helloButton.setDisable(disableButtons);;
+		byeButton.setDisable(disableButtons);
+		
+	}
+	
+	
 }
